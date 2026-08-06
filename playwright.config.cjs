ee3b2d0b85f7800a7f0fc45acbbc341d80bd9a5c
Roman Markovtsev <sns@rvm.cc>
2026-08-07 00:43:42 +0900

    Refactor SeaBirds into modular application features

---
+++ playwright.config.cjs
@@ -0,0 +1,7 @@
+const {defineConfig}=require('@playwright/test');
+module.exports=defineConfig({
+ testDir:'./tests',
+ timeout:30000,
+ use:{baseURL:'http://127.0.0.1:4175',headless:true},
+ webServer:{command:'node tests/server.cjs',url:'http://127.0.0.1:4175',reuseExistingServer:false}
+});
